0da5d174f34dc6edc70f477c74f411cbdf0c610b
AnatoliiZh <abcland2014@gmail.com>
2023-03-23 14:38:07 +0200

    add header and background slide

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    effect: "fade",
+});
